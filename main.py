7045f8a3171dd1dd9b23e759ed69a23c75119715
shlomo mica <shlomo.mica@outlook.com>
2023-11-25 21:16:21 +0200

    Finished login function with all the authetication

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
-import wtforms
-from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+import flask
+import flask_login
+from django.utils.http import url_has_allowed_host_and_scheme
+from wtforms import StringField, SubmitField, PasswordField
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory, current_app
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user,
-from wtforms.validators import ValidationError, DataRequired
-from wtforms import Form
-login_manager=LoginManager()
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret-key-goes-here'
 app.config['UPLOAD_FOLDER'] = 'static/files/'
+
+login_manager = LoginManager()
 login_manager.init_app(app)
+
 # CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy()
-db.init_app(app)
 
 
 # CREATE TABLE IN DB
-class User(db.Model):
+class User(db.Model,UserMixin):  # ADD usermixin from the example
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
 
 
+db.init_app(app)
 with app.app_context():
     db.create_all()
+
+
+@login_manager.user_loader
+def load_user(id):
+    print(id)
+    return User.query.get(int(id))
 
 
 @app.route('/')
@@ -37,42 +47,80 @@ def home():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    # if request.method == "POST":works also ...
-    new_user = User(name=request.form.get('name'),
-                    email=request.form.get('email'),
-                    password=request.form.get('password'))
-    exist = db.session.query(User).filter_by(name=new_user.name).first()
-    if exist:
-        print("noooooo")
+    # .is_authenticated:
+    if request.method == "POST":  #:works also ...
 
-    else:
-        new_user.password = generate_password_hash(new_user.password, method="pbkdf2", salt_length=2)
+        new_user = User(name=request.form.get('name'),
+                        email=request.form.get('email'),
+                        password=request.form.get('password'))
 
-        db.session.add(new_user)
-        db.session.commit()
-        first_time = new_user.name
-        return redirect(f'/secrets/{first_time}')
+        exist = db.session.query(User).filter_by(email=new_user.email).first()
+        if exist:
+            flash("Email address have already exist")
+            print("noooooo")
+
+        else:
+            new_user.password = generate_password_hash(new_user.password, method="pbkdf2", salt_length=2)
+
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            first_time = new_user.name
+            flask.flash('Your account has been created! You are now able to log in', 'success')
+            # return redirect(f'/secrets/{first_time}')
 
     return render_template('register.html')
 
 
-@app.route('/login',methods=['GET','POST'])
-
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
+    if request.method == 'POST':
+        exist_user = User.query.all()  # CALL ALL RECORDS IN DATABASE
+        user_login = User.query.filter_by(email=request.form.get('email')).first()
+        #load_user(user_login(user_login))
 
-    return render_template("login.html")
+        if user_login:
+            if check_password_hash(user_login.password, request.form.get('password')):
+                login_user(user_login,remember=True)
+                name_login = user_login.name
+                return redirect(url_for('secrets'))  # return redirect(f'/secrets/')  # TODO load_user(22)
+    return flask.render_template('login.html')
 
 
-@app.route('/secrets/<var>')
-def secrets(var):
-    var = var
-    return render_template("secrets.html", var=var)
+# print(request.form.get('email'))
+# print(request.form.get('password'))
+# print(exist_user[4].name, user_login.password)
+# next = flask.request.args.get('next')
+
+# # Login and validate the user.
+# # user should be an instance of your `User` class
+# #
+# flash('Logged in successfully.')
+#
+# next = flask.request.args.get('next')
+# # url_has_allowed_host_and_scheme should check if the url is safe
+# # for redirects, meaning it matches the request host.
+# # See Django's url_has_allowed_host_and_scheme for an example.
+# if not url_has_allowed_host_and_scheme(next, request.host):
+#     return flask.abort(400)
+#
+# return flask.redirect(next or flask.url_for('index'))
+
+
+@app.route('/secrets/')
+# @login_required
+def secrets():
+    return render_template("secrets.html", var=current_user.name)
 
 
 @app.route('/logout')
+@login_required
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('home'))
+
+
+
 
 
 @app.route('/download/<filename>')
